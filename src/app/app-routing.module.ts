a704f84efc82bffe861ef79c2abeecc8481a6047
Sebastian Riquelme <sebastianrh.sarh@gmail.com>
2020-07-08 13:00:15 -0400

    se crean las primeras rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './components/index/index.component';
+import { UserComponent } from './components/user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: '', pathMatch: 'full', redirectTo: ''},
+  { path: 'users', component: UserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
