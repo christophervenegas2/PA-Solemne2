54cbf93ae0801b28211ba3d7d956226a015a2da0
Sebastian Riquelme <sebastianrh.sarh@gmail.com>
2020-07-10 21:08:59 -0400

    se agregan estilos a la lista de posts

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PostsService } from 'src/app/services/posts.service';
 import { Post } from 'src/app/classes/post';
 import { Router } from '@angular/router';
+import { CommentsService } from 'src/app/services/comments.service';
 
 @Component({
   selector: 'app-post',
@@ -12,10 +13,12 @@ export class PostComponent implements OnInit {
 
   public posts: Post[];
   public userId: number;
+  // public comments: Comment[];
 
   constructor(
     private postsService: PostsService,
     private router: Router
+    // private commentsService: CommentsService
   ) { }
 
   public ngOnInit() {
@@ -29,6 +32,11 @@ export class PostComponent implements OnInit {
     this.postsService.getPostByUser(Number(this.userId)).then((data:Post[])=> {
       this.posts = data;
     });
+
+    // this.commentsService.getCommentByPost(Number(this.userId)).then((data:Comment[]) => {
+    //   this.comments = data;
+    //   console.log(this.comments);
+    // });
   }
 
   public viewPost(post: any) {
