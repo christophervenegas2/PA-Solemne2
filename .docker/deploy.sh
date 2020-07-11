5bce5ab5bbf0210e7fc1447677d90bd48a244727
Christopher Venegas <christophervenegas2@gmail.com>
2020-07-11 18:37:28 -0400

    Se conecta el deploy.sh a nuestra repo

--- .docker/deploy.sh
+++ .docker/deploy.sh
@@ -1,8 +1,8 @@
 # Traemos los cambios del repositorio
 git pull origin master
 # Traemos la ultima
-echo tfAHaoxguorGQD4RtPk4 | cat | docker login registry.gitlab.com --username acalvoa --password-stdin
-docker pull registry.gitlab.com/acalvoa/devops-test
+echo gCjJ4kQUrKh7w11j2EyH | cat | docker login registry.gitlab.com --username kerry --password-stdin
+docker pull registry.gitlab.com/sebastianrh.sarh/solemne2_g1
 # Hacemos deploy de la imagen
-cd /home/jenkins/devops-test/devops/.docker
+cd /home/kerry/solemne2_g1/.docker
 docker stack deploy -c docker.swarm.yml angular --with-registry-auth
